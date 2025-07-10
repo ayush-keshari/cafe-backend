91bb59607d5f68b65957741005df2342d6bcf454
ayush-keshari <ayushkeshari38@gmail.com>
2025-07-10 16:14:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRoute.js";
+import dotenv from 'dotenv';
+import cors from 'cors'
+dotenv.config();
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+const dbuser=encodeURIComponent(process.env.DBUSER);
+const dbpass=encodeURIComponent(process.env.DBPASS);
+// mongoose.connect("mongodb://localhost:27017/lpu").then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
+
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.4rzjzfg.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() =>   {
+  app.listen(8080, () => {
+    console.log("Server started");
+  });
+});
+
+app.use("/api/users", userRouter);
